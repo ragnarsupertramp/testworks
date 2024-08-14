d3056423c136c655b6655e874408075d65d5cb32
F4B07 <fabiangallardoo18@gmail.com>
2024-08-14 18:52:51 -0400

    integrando firebase para persistencia de datos 2

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyA-GViOkkcl4kzYg4djj3k7i2MUB8XIi6s",
-  authDomain: "testsworks.firebaseapp.com",
-  projectId: "testsworks",
-  storageBucket: "testsworks.appspot.com",
-  messagingSenderId: "123090865440",
-  appId: "1:123090865440:web:085253be8ac119edf51b45",
-  measurementId: "G-CG693H1Z4R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
